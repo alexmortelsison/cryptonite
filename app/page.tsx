17147804f9ecfc22b8818d8ed9931e68cc75267a
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2025-02-13 17:31:21 +0900

    added nextauth

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import { MoveRight } from "lucide-react";
 
 export default function Home() {
   return (
-    <div className="w-full flex flex-col justify-center items-center h-[70vh] max-w-6xl mx-auto">
-      <h1 className="text-primary text-6xl font-extrabold text-center tracking-tighter leading-tight">
+    <div className="w-full flex flex-col justify-center items-center h-[80vh] max-w-6xl mx-auto px-4 md:px-0">
+      <h1 className="text-primary text-2xl sm:text-6xl font-extrabold text-center tracking-tighter leading-none">
         Your Go-To Hub for Everything Crypto – Stay Informed, Stay Ahead.
       </h1>
       <p className="text-center pt-6 text-muted-foreground tracking-tighter">
